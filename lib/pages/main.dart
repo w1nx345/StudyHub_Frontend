df4166489e61ddfce21bf8718499963e44082608
DaveMufadhal <162690278+DaveMufadhal@users.noreply.github.com>
2024-05-26 20:19:43 +0700

    Add files via upload

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,17 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:studyhub/pages/profilePage.dart'; // Import your filter page
-import 'package:studyhub/pages/Filterpage.dart';
-import 'package:studyhub/pages/search.dart'; // Import your profile page
+import 'package:learn_hub/pages/chatlist_page.dart';
+import 'package:learn_hub/pages/filter_page.dart';
+import 'package:learn_hub/pages/splash_screen.dart';
+import 'package:learn_hub/pages/chat_Page.dart';
+import 'package:learn_hub/pages/settings_page.dart';
+import 'pages/login_page.dart';
+import 'package:learn_hub/pages/profile_page.dart';
+import 'package:learn_hub/pages/search.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SearchPage(),
+      debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+      routes: {
+    '/chatlist': (context) => ChatListPage(),
+    },
     );
   }
 }
+
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: ChatPage(
+//       )
+//     );
+//   }
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: SettingsPage(
+//           title: ('Settings'),
+//         )
+//     );
+//   }
+// }
+
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: FilterPageContent(),
+//     );
+//   }
+// }
