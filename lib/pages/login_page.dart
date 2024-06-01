54ce8202d534363e32c37c0f3ecfeb83e13db341
SushiBar1111 <fachry.altair18@gmail.com>
2024-06-01 14:10:05 +0700

    Finished login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,43 +1,71 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:learn_hub/components/my_button.dart';
 import 'package:learn_hub/components/my_textfield.dart';
 import 'package:learn_hub/components/square_tile.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
-
-  // text editing controllers
+  final storage = FlutterSecureStorage();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
-  // sign user in method
-  void signUserIn() {
+  void signUserIn(BuildContext context) async {
     String enteredEmail = emailController.text;
     String enteredPassword = passwordController.text;
+
+    try {
+      final response = await http.post(
+        Uri.parse('http://10.0.2.2:8000/login/'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'email': enteredEmail,
+          'password': enteredPassword
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body);
+        final accessToken = jsonData['access'];
+        final refreshToken = jsonData['refresh'];
+
+        await storage.write(key: "access", value: accessToken);
+        await storage.write(key: "refresh", value: refreshToken);
+
+        Navigator.pushNamed(context, '/chatlist');
+      } else {
+        final jsonData = jsonDecode(response.body);
+        // Handle login error here (e.g., show a message to the user)
+        throw Exception(jsonData);
+      }
+    } catch (e) {
+      // Handle error (e.g., show a message to the user)
+      print('Error logging in: $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomInset: true,
       backgroundColor: const Color(0xFF2F27CE),
-      body:SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(height: 50),
-              // logo
-          Image.asset(
-            'lib/images/Study Hub Logo.png',
-            height: 250,
-            width: 250,
-          ),
-
+              Image.asset(
+                'images/Study Hub Logo.png',
+                height: 250,
+                width: 250,
+              ),
               const SizedBox(height: 50),
-
               const SizedBox(height: 25),
-
               Column(
                 children: <Widget>[
                   SizedBox(
@@ -56,15 +84,11 @@ class LoginPage extends StatelessWidget {
                   ),
                 ],
               ),
-
-              // E-mail textfield
               MyTextField(
                 controller: emailController,
                 obscureText: false,
               ),
-
               const SizedBox(height: 5),
-
               Column(
                 children: <Widget>[
                   SizedBox(
@@ -85,28 +109,17 @@ class LoginPage extends StatelessWidget {
                   ),
                 ],
               ),
-
-              // password textfield
               MyTextField(
                 controller: passwordController,
                 obscureText: true,
               ),
-
               const SizedBox(height: 10),
-
               const SizedBox(height: 25),
-
-              // sign in button
               MyButton(
-                onTap: () {
-        Navigator.pushNamed(context, '/chatlist');
-        },
+                onTap: () => signUserIn(context),
                 width: 200,
               ),
-
               const SizedBox(height: 50),
-
-              // or continue with
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Row(
@@ -122,8 +135,9 @@ class LoginPage extends StatelessWidget {
                       child: Text(
                         'Or Sign Up Using',
                         style: TextStyle(
-                            fontFamily: 'OpenSans',
-                            color: Colors.white),
+                          fontFamily: 'OpenSans',
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                     Expanded(
@@ -135,47 +149,42 @@ class LoginPage extends StatelessWidget {
                   ],
                 ),
               ),
-
               const SizedBox(height: 50),
-
-              // google sign in buttons
               const Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // google button
-                  SquareTile(imagePath: 'lib/images/google.png'),
-
+                  SquareTile(imagePath: 'images/google.png'),
                   SizedBox(width: 25),
-
-                  // facebook button
-                  SquareTile(imagePath: 'lib/images/facebook.png')
+                  SquareTile(imagePath: 'images/facebook.png')
                 ],
               ),
-
               const SizedBox(height: 50),
-
-              // not a member? register now
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
                     'If you haven’t any account?',
                     style: TextStyle(
-                        fontFamily: 'OpenSans',
-                        color: Colors.white
+                      fontFamily: 'OpenSans',
+                      color: Colors.white,
                     ),
                   ),
                   const SizedBox(width: 4),
-                  const Text(
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/signup'); // ke sign up page
+                    },
+                  child: const Text(
                     'Sign Up',
                     style: TextStyle(
                       color: Colors.blue,
                       fontWeight: FontWeight.bold,
-                      fontFamily: 'OpenSans'
+                      fontFamily: 'OpenSans',
+                      ),
                     ),
                   ),
                 ],
-              )
+              ),
             ],
           ),
         ),
@@ -183,4 +192,3 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
-
